8d62f81f14305c241fbbab70d2ca4b6780701dd4
Markus <holmbergmarkus1997@gmail.com>
2019-11-14 12:19:51 +0000

    Tried fixing validation errors again

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ from app import db, login
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
                      db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
+                    )
 
 class User(UserMixin, db.Model):
     """
